9f3dd4bbf219cad1084a1842e121bd571683d549
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-24 09:49:54 +0700

    +description of methods

--- index.d.ts
+++ index.d.ts
@@ -4,14 +4,42 @@ export interface Opt {
   duration: number;
 }
 declare function cache(options: Partial<Opt> = {}): {
+  /**
+   * Insert data
+   */
   put: (name: string, data: any, callback: any) => any;
+  /**
+   * Retrieve data
+   */
   get: <T>(name: string, callback: (err: Error, data: T) => any) => any;
+  /**
+   * Delete data
+   */
   delete: (name: string, callback: any) => void;
-  putSync: (name: string, data: any) => void;
-  getSync: <T>(name: string) => T;
+  /**
+   * Delete data synchronous
+   */
   deleteSync: (name: string) => void;
+  /**
+   * Insert data synchronous
+   */
+  putSync: (name: string, data: any) => void;
+  /**
+   * Retrieve data synchronous
+   */
+  getSync: <T>(name: string) => T;
+
+  /**
+   * Getting available keys
+   */
   keys: (callback: (keys: string[]) => any) => any;
+  /**
+   * Getting available keys synchronous
+   */
   keysSync: () => string[];
+  /**
+   * delete the folder and files of a persistent cache
+   */
   unlink: (callback: any) => any;
 };
 export default cache;
